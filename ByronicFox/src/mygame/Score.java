e57a0aa65aa28a02bfe8add1648009c609a0b076
richardwijaya <32352818+richardwijaya@users.noreply.github.com>
2017-11-10 22:10:30 +0700

    Create Score.java

---
+++ ByronicFox/src/mygame/Score.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mygame;
+
+/**
+ *
+ * @author Richard
+ */
+public class Score{
+
+    private int score;
+    
+    public Score(){
+      score = 0;
+    }
+    
+}
